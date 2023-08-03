5dddea66bbff0148713d144485c74364061cd578
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-08-03 16:33:36 +0600

    Added Banner Area without responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -7,16 +7,21 @@ const Banner = () => {
             <div className="banner-wrapper">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-7">
                             <div className="banner-left">
                                 <picture>
-                                    <img src="./assets/homepage-3dcharacter_1.webp" alt="" />
+                                    <img width='1100px' height='1530px' src="./assets/homepage-3dcharacter_1.webp" alt="banner" />
                                 </picture>
                             </div>
                         </div>
-                        <div className="col-lg-6">
+                        <div className="col-lg-5">
                             <div className="banner-right">
                                 <h3>Receive Online SMS with Virtual Number Service</h3>
+                                <p>Use our disposable phone number service to verify your accounts. Cheap and reliable SMS verification service. Pay with credit/debit card or cryptocurrency.</p>
+                                <div className="banner-right--inner">
+                                    <a href="##" className='btn1'>Create an Account</a>
+                                    <a href="##" className='btn2'>View Demo</a>
+                                </div>
                             </div>
                         </div>
                     </div>
